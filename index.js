1b70f097b64b0d24e10369447f41d0f7d8fb68af
BobTheBuilder786 <106140535+BobTheBuilder786@users.noreply.github.com>
2022-05-24 15:07:55 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
 const axios = require('axios').default;
 
-setInterval(
 
+
+function everyTime() {
     
-
 // Make a request for a user with a given ID
-
 var array = ["AdviceAnimals","MemeEconomy","MemeCemetery","memes","dankmemes","PrequelMemes","funny","teenagers"]
-
 var red = array[Math.floor(Math.random() * array.length)]
-
 axios.get('https://meme-api.herokuapp.com/gimme/'+ red)
-
   .then(function (response) {
-
     // handle success
-
     var ups = response.data.ups
-
     var url = response.data.url
-
     var cap = response.data.title
-
   
-
   if (ups> 500){
-
   console.log(`Meme Found: caption: ${cap} subreddit:${red} url:${url}`)
-
   
-
   }
-
   })
 
-    , 5000);
+}
+
+var myInterval = setInterval(everyTime, 5000);
+    
