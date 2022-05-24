af05c0b83af435474557322a673a5501e1d2ca23
BobTheBuilder786 <106140535+BobTheBuilder786@users.noreply.github.com>
2022-05-24 14:52:57 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const axios = require('axios').default;
+
+setTimeout(function(){
+
+    
+
+// Make a request for a user with a given ID
+
+var array = ["AdviceAnimals","MemeEconomy","MemeCemetery","memes","dankmemes","PrequelMemes","funny","teenagers"]
+
+var red = array[Math.floor(Math.random() * array.length)]
+
+axios.get('https://meme-api.herokuapp.com/gimme/'+ red)
+
+  .then(function (response) {
+
+    // handle success
+
+    var ups = response.data.ups
+
+    var url = response.data.url
+
+    var cap = response.dsta.title
+
+  
+
+  if (ups> 500){
+
+  console.log(`Meme Found: caption: ${cap} subreddit:${red} url:${url}`)
+
+  
+
+  }
+
+  })
+
+    }, 5000);
